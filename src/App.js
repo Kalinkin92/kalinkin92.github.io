b5b29661dde4e7db1464b6b6599f1a824d0b2163
Igor Kalinkin <kalinkin92@gmail.com>
2018-04-26 11:32:52 +0300

    СЛАВА УКРАИНИ

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+const logo = '/ms-icon-144x144.png';
+const gif = '/1490615640_1491-flag-ukrainy.gif';
+const gimn = '/zasluzhennyy-hor-ukrainy-gimn-ukrainy.mp3';
 
 class App extends Component {
   render() {
@@ -8,11 +11,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Хохло кiт</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="App-body">
+              <p className="App-intro">
+                  ибав вас в рiт
+              </p>
+              <img src={gif} alt="СЛАВА УРАИНИ" />
+              <p className="App-intro">Слава Україні!
+              </p>
+              <audio controls>
+                      <source src={gimn} type="audio/mpeg"/>
+                          Your browser does not support the audio element.
+              </audio>
+
+          </div>
       </div>
     );
   }
