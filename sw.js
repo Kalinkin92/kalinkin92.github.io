98f65c39709657f8d3672f41a02e3444025bd1e6
Igor Kalinkin <kalinkin92@gmail.com>
2018-04-26 11:53:58 +0300

    fix again

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
 importScripts('sw-toolbox.js');
-toolbox.precache(["index.html","/pwa-ex/static/css/main.938406eb.css"]);
+toolbox.precache(["index.html","static/css/main.c17080f1.css"]);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
